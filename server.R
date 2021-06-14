811ae527b5bf019e19f3f8f287a956aced747de2
kmnhngyn <s0563958@htw-berlin.de>
2021-06-14 17:56:55 +0200

    dynamic table view; calculate mean and deviation

--- server.R
+++ server.R
@@ -5,22 +5,89 @@
 # then define your outputs (i.e. plots) using input from the UI.
 
 # Load libraries, data
+library(dplyr)
 #library(ggplot2)
 #characters <- read.csv(url("www"))
 
+aquarium_df <- read.csv(file = 'data/aquarium.csv', header = TRUE, sep = ",")
+nitrat_df <- aquarium_df["Nitrat.NO3"]
+# co2_df <- aquarium_df["CO2"]
+# nitrat_c02 <- aquarium_df[, c("Nitrat.NO3","CO2")]
 
 # Create server
 server <- function(input, output) {
   
+  # Side panel in page 2
   output$selected_var <- renderText({
     paste("Deine Auswahl ist ", input$var)
   })
   
-  output$data_table<- DT::renderDataTable({
-    df <- read.csv(file = 'data/aquarium.csv', header = TRUE, sep = ",")
-    DT::datatable(df)
+  # Main content in page 2
+  
+  output$mittelwert <- renderText({
+    if(input$var == "Alle Werte"){
+      paste ("")
+    } else {
+      if(input$var == "Nitrat-NO3"){
+        mean <- mean(aquarium_df$Nitrat.NO3)
+      } else if(input$var == "CO2"){
+        mean <- mean(aquarium_df$CO2)
+      }
+      paste("Der Mittelwert von ", input$var, " beträgt: ", mean)
+    }
   })
   
+  output$standardabweichung <- renderText({
+    if(input$var == "Alle Werte"){
+      paste("")
+    } else{
+      if(input$var == "Nitrat-NO3"){
+        sd <- sd(aquarium_df$Nitrat.NO3)
+      }
+      if(input$var == "CO2"){
+        sd <- sd(aquarium_df$CO2)
+      }
+      paste("Die Standardabweichung von ", input$var, " beträgt: ", sd)
+    }
+  })
+  
+  # dynamic output table
+  reactive_df <- reactive({
+    if(input$var == "Alle Werte"){
+      table_df <- aquarium_df
+    }
+    if(input$var == "Nitrat-NO3"){
+      table_df <- subset(aquarium_df, select = c("Date","Nitrat.NO3"))
+    }
+    if(input$var == "CO2"){
+      table_df <- subset(aquarium_df, select = c("Date","CO2"))
+    }
+    return(table_df)
+  })
+  
+  # table with all data
+  output$all_table<- DT::renderDataTable({
+    # aquarium_df <- read.csv(file = 'data/aquarium.csv', header = TRUE, sep = ",")
+    # DT::datatable(aquarium_df)
+    # nitrat_df <- aquarium_df["Nitrat.NO3"]
+    paste(h2("Tabelle"))
+    DT::datatable(reactive_df())
+  })
+  
+  #table only nitrat
+  output$nitrat_table<- DT::renderDataTable({
+    # aquarium_df <- read.csv(file = 'data/aquarium.csv', header = TRUE, sep = ",")
+    # DT::datatable(aquarium_df)
+    # nitrat_df <- aquarium_df["Nitrat.NO3"]
+    DT::datatable(nitrat_df)
+  })
+  
+  # Plot
+  output$aquarium_plot <- renderPlot({
+    plot(nitrat_df)
+  })
+  
+  # Content in page 3
   df_products_upload <- reactive({
     inFile <- input$target_upload
     if (is.null(inFile))
--- ui.R
+++ ui.R
@@ -11,29 +11,41 @@ intro_panel <- tabPanel(
   
   #  img(src = "img source"),
   h2("Aufgabe der Hausarbeit"),
-  p("Aufgabe der Hausarbeit ist, eine interaktive Visualisierung mit Shiny in R zu bauen, welche ein statistisches Konzept aus der Vorlesung demonstriert. Beispiele wären die Regel von Bayes, Konfidenzintervalle oder Hypothesentests. So könnten Sie z.B. Daten auf Knopfdruck künstlich erzeugen und visualisieren und dabei Parametereinstellungen wie die Größe des Datensatzes, Wahrscheinlichkeitsverteilung, Streuung, Konfidenz- oder Signifikanzniveau ermöglichen. Darauf basierend könnten Sie z.B. Konfidenzintervalle berechnen oder Hypothesentests durchführen und die Ergebnisse visualisieren. Die Shiny-App könnte dann interaktiv den Einfluss der Parameter auf die Ergebnisse demonstrieren wie etwa den Einfluss von Stichprobengröße und Konfidenz-/Signifikanzniveau auf die Größe des Konfidenzintervalls oder auf das Ergebnis des Hypothesentests. 
-    Als Alternative zu künstlichen Daten können Sie auch echte Daten (z.B. COVID-19-Daten vom Robert-Koch-Institut) benutzen - entweder komplett oder Stichproben daraus.
-    Ich erwarte Visualisierungen von Daten und Ergebnissen, welche z.B. durch Drop-Down-Menüs, Slider und Knöpfe verändert werden können, um die Daten und Ergebnisse zu explorieren. Weiterhin können Sie sich von interaktiven Visualisierungen im Internet inspirieren lassen und diese teilweise in Shiny nachbauen."), 
+  p(
+  "Aufgabe der Hausarbeit ist, eine interaktive Visualisierung mit Shiny in R zu bauen, welche ein statistisches Konzept aus der Vorlesung demonstriert.",br(),
+  "Beispiele wären die Regel von Bayes, Konfidenzintervalle oder Hypothesentests.", br(),
+  "So könnten Sie z.B. Daten auf Knopfdruck künstlich erzeugen und visualisieren und dabei Parametereinstellungen", br(),
+  "wie die Größe des Datensatzes, Wahrscheinlichkeitsverteilung, Streuung, Konfidenz- oder Signifikanzniveau ermöglichen.", br(),
+  "Darauf basierend könnten Sie z.B. Konfidenzintervalle berechnen oder Hypothesentests durchführen und die Ergebnisse visualisieren.", br(),
+  "Die Shiny-App könnte dann interaktiv den Einfluss der Parameter auf die Ergebnisse demonstrieren wie etwa den Einfluss", br(),
+  "von Stichprobengröße und Konfidenz-/Signifikanzniveau auf die Größe des Konfidenzintervalls oder auf das Ergebnis des Hypothesentests.",br(),br(), 
   
-  p("Wichtig: Geben Sie wie bei den bewerteten Aufgaben bitte immer die Quellen an, egal ob von Daten, Code, Beschreibungen oder anderen Visualisierungen. Einfaches Kopieren einer Visualisierung durch Kopieren des Codes werte ich als Plagiat, aber inhaltliche Inspiration mit eigener Implementierung ist in Ordnung."), 
+  "Als Alternative zu künstlichen Daten können Sie auch echte Daten (z.B. COVID-19-Daten vom Robert-Koch-Institut) benutzen - entweder komplett oder Stichproben daraus.", br(),br(),
   
-  p("Unter https://shiny.rstudio.com/tutorial/ finden Sie ausführliche Tutorials zum Bauen von Shiny-Apps mit vielen Beispielen. Links zu anderen Beispielen, welche wir in der Vorlesung besprochen haben, finden Sie in unserem Moodlekurs (zu den Themen Würfelwahrscheinlichkeit und Genauigkeit von COVID-19-Tests)."), 
+  "Ich erwarte Visualisierungen von Daten und Ergebnissen, welche z.B. durch Drop-Down-Menüs, Slider und Knöpfe verändert werden können, um die Daten und Ergebnisse zu explorieren.",br(),
+  "Weiterhin können Sie sich von interaktiven Visualisierungen im Internet inspirieren lassen und diese teilweise in Shiny nachbauen.",br(),br(),
+  
+  "Wichtig: Geben Sie wie bei den bewerteten Aufgaben bitte immer die Quellen an, egal ob von Daten, Code, Beschreibungen oder anderen Visualisierungen.",br(),
+  "Einfaches Kopieren einer Visualisierung durch Kopieren des Codes werte ich als Plagiat, aber inhaltliche Inspiration mit eigener Implementierung ist in Ordnung.",br(),br(), 
+  
+  "Unter https://shiny.rstudio.com/tutorial/ finden Sie ausführliche Tutorials zum Bauen von Shiny-Apps mit vielen Beispielen.",br(),
+  "Links zu anderen Beispielen, welche wir in der Vorlesung besprochen haben, finden Sie in unserem Moodlekurs (zu den Themen Würfelwahrscheinlichkeit und Genauigkeit von COVID-19-Tests)."), 
   
   h4("Abgabetermin ist der 28.06.2021 um 23:59."), 
-  
-  h4("Abgabe in Ihrer Gruppe:"),
-  p("Quellcode der Shiny-App in Moodle hochladen. Stellen Sie sicher, dass dieser ausführbar ist und geben Sie die Quellen in der laufenden Shiny-App sichtbar an. 
-    Stellen Sie Ihre Shiny-App auf https://www.shinyapps.io zur Verfügung und geben den Link an."),
-  
-  p("Beschreiben Sie die interaktive Visualisierung auf einer Seite Text mit Quellenangaben und geben das Dokument in Moodle ab."),
-  
-  p("Präsentation: Am 30.06.2021 müssen Sie in den Übungen Kurzpräsentationen Ihrer Shiny-Apps geben, alle Gruppenmitglieder müssen anwesend sein und präsentieren."),
-  
   br(),
-  h4("Quellen"),
-  p(a(href = "https://github.com/kmnhngyn/statistik", "Eigene GitHub Repo")),
-  p(a(href = "https://riptutorial.com/shiny/example/32449/uploading-csv-files-to-shiny", "CSV laden und darstellen (Page 3)"))
+  h4("Abgabe in Ihrer Gruppe:"),
+  p("Quellcode der Shiny-App in Moodle hochladen.", br(),
+  "Stellen Sie sicher, dass dieser ausführbar ist und geben Sie die Quellen in der laufenden Shiny-App sichtbar an.", br(),
+  "Stellen Sie Ihre Shiny-App auf https://www.shinyapps.io zur Verfügung und geben den Link an.",br(),br(),
   
+  "Beschreiben Sie die interaktive Visualisierung auf einer Seite Text mit Quellenangaben und geben das Dokument in Moodle ab.",br(),br(),
+  
+  "Präsentation: Am ", strong("30.06.2021"), "müssen Sie in den Übungen Kurzpräsentationen Ihrer Shiny-Apps geben, alle Gruppenmitglieder müssen anwesend sein und präsentieren."),
+  
+  h2("Quellen"),
+  pre (a(href = "https://github.com/kmnhngyn/statistik", "Eigene GitHub Repo"),
+       a(href = "https://riptutorial.com/shiny/example/32449/uploading-csv-files-to-shiny", "CSV laden und darstellen (Page 3)")
+      )
 )
 
 # ------------------------------------------------------------------------------
@@ -43,28 +55,39 @@ daten_sidebar <- sidebarPanel(
   selectInput(
     # select widget var
     "var",
-    label = "Auswahl",
+    label = "Was möchtest du anzeigen lassen?",
     #select_values = colnames(data),
     #choices = colnames(data),
     #selected = "Speed"
-    choices = c( "Auswahl 1",
-                 "Auswahl 2",
-                 "Auswahl 3"),
-    selected = "Keine Auswahl"
+    choices = c( "Alle Werte",
+                 "Nitrat-NO3",
+                 "CO2"),
+    selected = "Nitrat"
   )
 )
 
-main_content <- mainPanel(
+daten_main <- fluidPage(
   #plotOutput("plot")
-  textOutput("selected_var"),
-  DT::dataTableOutput("data_table")
+  # textOutput("selected_var"),
+  # h2("Tabellenausgabe"),
+  DT::dataTableOutput("all_table"),
+  br(),
+  h2("Hier werden berechnete Daten ausgegeben."),
+  textOutput("mittelwert"),
+  textOutput("standardabweichung"),
+  textOutput("untergrenze"),
+  textOutput("obergrenze"),
+  # TODO: diese ansicht dynamisch machen
+  # h3("Tabelle für Nitrat"),
+  # DT::dataTableOutput("nitrat_table")
+  # plotOutput("aquarium_plot")
 )
 
 daten_panel <- tabPanel(
   "Daten",
   titlePanel("Daten"),
   sidebarLayout(
-    daten_sidebar, main_content
+    daten_sidebar, daten_main
   )
 )
 
